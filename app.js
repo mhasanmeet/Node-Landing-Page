8d1d15b872687a1ee55fcea6c4d2c199326eadc0
mhasan <mhasan.meet@gmail.com>
2022-11-22 13:00:18 +0600

    added controller

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { getIndex, getAbout } = require('./controller')
 const app = express()
 const PORT = 5600
 
@@ -9,6 +10,17 @@ app.listen(PORT, ()=>{
 app.get('/', (req, res)=>{
     res.send('Hello World')
 })
+
+//get css rendered file
+app.use(express.static(__dirname + '/public'))
+
+//template engine location set
+app.set('view engine', 'ejs')
+
+//get router from controller
+app.get('/', getIndex)
+
+app.get('/about', getAbout)
 
 
 
--- controller.js
+++ controller.js
@@ -1,1 +1,13 @@
 //index Route
+exports.getIndex = (req, res) =>{
+    res.render('pages/index', {
+        title: `CodegNet: We Develop Software`
+    })
+}
+
+//about Route
+exports.getAbout = (req, res) => {
+    res.render('pages/about', {
+        title: `About Us`
+    })
+}
